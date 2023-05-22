f0763175c42e944f30dec989745de3a7664a48db
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-05-22 13:38:50 +0200

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/connexion.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <section class="login-form">
+        <form action="" method="get">
+            <input type="text" placeholder="Nom d'utilisateur">
+            <input type="password" placeholder="Mot de passe">
+            <button class="login">Se connecter</button>
+        </form>
+    </section>
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/inscription.css" type=text/css>
+    <title>Inscription</title>
+</head>
+<body>
+    <section class="registration-form">
+        <form action="" method="get">
+            <input type="text" placeholder="Nom d'utilisateur">
+            <input type="text" placeholder="Prenom">
+            <input type="text" placeholder="Nom">
+            <input type="password" placeholder="Mot de passe">
+            <input type="password" placeholder="Confirmation Mot de passe">
+            <button class="register">S'inscrire</button>
+        </form>
+    </section>
+</body>
+</html>
